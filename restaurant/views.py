0c6a1adb16eb22655f662ee276bb06de50893479
waseem <waseem.96.ra@gmail.com>
2023-09-27 10:00:08 +0200

    Create booking_list view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Booking
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def booking_list(request):
+    bookings = Booking.objects.all()
+    return render(request, 'booking/booking_list.html', {'bookings': bookings})
