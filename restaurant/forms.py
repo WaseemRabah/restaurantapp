7c0b6fcf751ebc84913bc6ecb93d97b241c9f52c
waseem <waseem.96.ra@gmail.com>
2023-09-27 17:55:18 +0200

    Create ContactForm in forms.py

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Booking
+from .models import Booking, ContactMessage
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['name', 'email', 'num_people', 'date', 'time', 'occasion']
+
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactMessage
+        fields = ['name', 'email', 'subject', 'message']
