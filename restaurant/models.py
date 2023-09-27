68ac753bfe50bd35ff9c7dab9d4c90b8919c880d
waseem <waseem.96.ra@gmail.com>
2023-09-27 09:41:35 +0200

    Create Booking model

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from datetime import datetime, time
+from django.shortcuts import render
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    num_people = models.PositiveIntegerField(default=1)
+    date = models.DateTimeField()
+    time = models.TimeField()
+    OCCASION_CHOICES = [
+        ('1', 'Enjoy my self'),
+        ('2', 'Date'),
+        ('3', 'Birthday'),
+        ('4', 'Anniversary'),
+        ('5', 'Party'),
+    ]
+    occasion = models.CharField(max_length=1, choices=OCCASION_CHOICES, default='1')
+    def __str__ (self):
+        return self.name
