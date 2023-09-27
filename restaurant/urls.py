9fa53af47ec81c286db43f0fd641fc0a4baab644
waseem <waseem.96.ra@gmail.com>
2023-09-27 18:10:03 +0200

    Add needed urls to urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('create_booking/', views.create_booking, name='create_booking'),  
     path('update_booking/<int:pk>/', views.update_booking, name='update_booking'),
     path('delete_booking/<int:pk>/', views.delete_booking, name='delete_booking'),
-    path('booking_detail/', views.booking_detail, name='booking_detail'),  
+    path('booking_detail/', views.booking_detail, name='booking_detail'),
+    path('contact/', views.contact, name='contact'),
+    path('contact_thank_you/', views.contact_thank_you, name='contact_thank_you'),
 ]
