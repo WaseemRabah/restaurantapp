3b1fe00c17fad01a7c1b1cd47c9c517bba2cfeea
waseem <waseem.96.ra@gmail.com>
2023-09-27 10:09:25 +0200

    Create create_booking view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Booking
+from .forms import BookingForm
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,14 @@ def home(request):
 def booking_list(request):
     bookings = Booking.objects.all()
     return render(request, 'booking/booking_list.html', {'bookings': bookings})
+
+
+def create_booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_list')
+    else:
+        form = BookingForm()
+    return render(request, 'booking/booking_form.html', {'form': form})
