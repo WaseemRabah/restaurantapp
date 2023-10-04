c10a16fe83e638f14ebbb17a8e0617e4d0c26285
waseem <waseem.96.ra@gmail.com>
2023-10-04 18:40:14 +0200

    Create loginPage and logoutUser views

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,6 +3,7 @@ from .models import Booking
 from .forms import BookingForm, ContactForm, CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -23,6 +24,30 @@ def registerPage(request):
 
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+            
+
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 
 def booking_list(request):
