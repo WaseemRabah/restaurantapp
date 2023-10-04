3c5f63e2daa184e3e4d78eeedeb1b8580b0df52f
waseem <waseem.96.ra@gmail.com>
2023-10-04 18:31:29 +0200

    Create registerPage view

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Booking
-from .forms import BookingForm, ContactForm
+from .forms import BookingForm, ContactForm, CreateUserForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user=form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'accounts/register.html', context)
 
 
 def booking_list(request):
