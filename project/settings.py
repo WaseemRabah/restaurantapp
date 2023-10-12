fb0e67e145464d4736ab5c3154f167b00aad5fca
waseem <waseem.96.ra@gmail.com>
2023-10-12 13:07:57 +0200

    Update DEBUG setting

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']=='True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = []
 
